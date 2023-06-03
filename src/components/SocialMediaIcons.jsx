0571da319b9ea45337f0c374150c6993072fc492
ayush-dhanker <ayush.dhanker@gmail.com>
2023-06-03 13:24:22 +0530

    first commit

---
+++ src/components/SocialMediaIcons.jsx
@@ -0,0 +1,49 @@
+const SocialMediaIcons = () => {
+  return (
+    <div className="flex justify-center md:justify-start my-10 gap-7  align-item">
+      <a
+        className="hover:opacity-50 transition duration-500 bg-deep-blue"
+        href="https://www.linkedin.com/in/ayush-dhanker-55a031223/"
+        target="_blank"
+        rel="noreferrer"
+      >
+        <img alt="linkedin-link" src="../assets/linkedin.png" />
+      </a>
+      <a
+        className="hover:opacity-50 transition duration-500"
+        href="https://github.com/ayush-dhanker"
+        target="_blank"
+        rel="noreferrer"
+        style={{ height: "30px", width: "50px", paddingTop: "4px" }}
+      >
+        <img alt="github-link" src="../assets/github_logo.png" />
+      </a>
+      {/* <a
+        className="hover:opacity-50 transition duration-500 bg-deep-blue"
+        href="https://www.twitter.com"
+        target="_blank"
+        rel="noreferrer"
+      >
+        <img alt="twitter-link" src="../assets/twitter.png" />
+      </a>
+      <a
+        className="hover:opacity-50 transition duration-500 bg-deep-blue"
+        href="https://www.facebook.com"
+        target="_blank"
+        rel="noreferrer"
+      >
+        <img alt="facebook-link" src="../assets/facebook.png" />
+      </a>
+      <a
+        className="hover:opacity-50 transition duration-500 bg-deep-blue"
+        href="https://www.instagram.com"
+        target="_blank"
+        rel="noreferrer"
+      >
+        <img alt="instagram-link" src="../assets/instagram.png" />
+      </a> */}
+    </div>
+  );
+};
+
+export default SocialMediaIcons;
---
+++ src/scenes/Footer.jsx
@@ -0,0 +1,21 @@
+import SocialMediaIcons from "../components/SocialMediaIcons";
+
+const Footer = () => {
+  return (
+    <footer className="h-64 bg-gradient-to-r from-indigo-500 from-10% via-sky-500 via-30% to-blue to-90% pt-10">
+      <div className="w-10/12 mx-auto">
+        <SocialMediaIcons />
+        <div className="md:flex justify-center md:justify-between text-center ">
+          <p className="font-playfair font-semibold text-2xl text-yellow">
+            AYUSH DHANKER
+          </p>
+          <p className="font-playfair text-md text-yellow">
+            ©2023. All Rights Reserved.
+          </p>
+        </div>
+      </div>
+    </footer>
+  );
+};
+
+export default Footer;
