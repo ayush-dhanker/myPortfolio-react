6516a2fb4e97afb0484e000eba437e36d169224e
ayush-dhanker <87539370+ayush-dhanker@users.noreply.github.com>
2025-04-01 15:57:33 +0200

    Update Footer.jsx

--- src/scenes/Footer.jsx
+++ src/scenes/Footer.jsx
@@ -10,7 +10,7 @@ const Footer = () => {
             AYUSH DHANKER
           </p>
           <p className="font-playfair text-md text-yellow">
-            ©2023. All Rights Reserved.
+            ©2025. All Rights Reserved.
           </p>
         </div>
       </div>
